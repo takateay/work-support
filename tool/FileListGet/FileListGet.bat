d3f082cfc6990957de3a85e89158bce2d257ed25
kahatato <28295782+kahatato@users.noreply.github.com>
2020-03-28 13:41:34 +0900

    ツール追加

--- tool/FileListGet/FileListGet.bat
+++ tool/FileListGet/FileListGet.bat
@@ -3,6 +3,6 @@ set today=%date:~0,4%%date:~5,2%%date:~8,2%
 set time2=%time: =0%
 set totime=%time2:~0,2%%time2:~3,2%%time2:~6,2%
 For %%i In (%*) Do (
-  TYPE %%i >> merge_%today%%totime%.csv
-  ECHO %%i >> merge_%today%%totime%.log
+  dir /b /s %%i >> FileList_%today%%totime%.txt
+  echo %%i >> FileList_%today%%totime%.log
 )
